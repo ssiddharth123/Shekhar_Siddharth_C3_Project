7b38fd3c9cfda16c9d88d835744319805fabf07d
ssiddharth123 <sid.shekhar01@gmail.com>
2021-10-20 21:52:40 +0530

    First commit

---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,88 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTest {
+    Restaurant restaurant;
+    List<Item> itemList = new ArrayList<Item>();
+
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+    @BeforeEach
+    public void beforeEachTest(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+    }
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+
+        Restaurant restaurantSearched = Mockito.spy(restaurant);
+        LocalTime currentTime = LocalTime.parse("13:00:00");
+        Mockito.when(restaurantSearched.getCurrentTime()).thenReturn(currentTime);
+        assertEquals(true,restaurantSearched.isRestaurantOpen());
+
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+
+        Restaurant restaurantSearched = Mockito.spy(restaurant);
+        LocalTime currentTime = LocalTime.parse("08:00:00");
+        Mockito.when(restaurantSearched.getCurrentTime()).thenReturn(currentTime);
+        assertEquals(false,restaurantSearched.isRestaurantOpen());
+       
+
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    // C3_Project Part 3: Adding a Feature using TDD
+    @Test
+    public void when_selecting_items_in_menu_total_order_value_should_be_returned(){
+        itemList = restaurant.getMenu();
+        assertEquals(388, restaurant.orderValue(itemList));
+    }
+
+    @Test
+    public void when_no_items_were_selected_in_menu_total_order_value_should_be_zero(){
+        double totalValue = restaurant.orderValue(itemList);
+        assertEquals(0, totalValue);
+    }
+}
